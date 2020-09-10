3a449fe1ba8fd4a7dd917309d980e61da4ad33e9
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-09-10 04:51:30 +0200

    update package-info

--- src/main/java/net/jamu/complex/package-info.java
+++ src/main/java/net/jamu/complex/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Basic implementation of a complex number type for double-precision
+ * Basic implementation of a mutable complex number type for double-precision
  * ({@link net.jamu.complex.Zd}) and single-precision
  * ({@link net.jamu.complex.Zf}) numbers. Both implementations are mutable.
  */
