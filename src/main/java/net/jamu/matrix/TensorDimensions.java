543ed24afa0d42e68a6856a8f3b8b3af588381f3
stefan-zobel <stefan-zobel@users.noreply.github.com>
2023-07-26 00:15:56 +0200

    add TensorDimensions interface

---
+++ src/main/java/net/jamu/matrix/TensorDimensions.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2023 Stefan Zobel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.jamu.matrix;
+
+/**
+ * Basic tensor properties mostly related to the dimension of a tensor.
+ */
+public interface TensorDimensions extends Dimensions {
+
+    /**
+     * Returns the depth (&gt;= 1) of this tensor.
+     * 
+     * @return the depth of this tensor
+     */   
+    public int numDepth();
+}
